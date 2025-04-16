e42dd501e858abad10b78df5c8788985e5d34d28
matheuscezar <matheuscezar@hotmail.com>
2025-04-15 21:16:59 -0300

    Create main.cpp

---
+++ 3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 100; i++) {
+        if (i % 3 ==0 && i % 5 == 0){
+            cout << "FooBaa" << endl;
+        }
+        else if (i % 3 == 0){
+            cout << "Foo" << endl;
+        }
+        else if (i % 5 == 0){
+            cout << "Foo" << endl;
+        }
+    }
+}
